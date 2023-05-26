0631e9bdabfc2440dcd936c5a1a9e9e04761743f
HaloYangDS <122417835+HaloYangDS@users.noreply.github.com>
2023-05-26 14:19:08 +0900

    Delete drawing_app/prototype directory

--- drawing_app/prototype/paintapp/static/paintapp/js/search.js
+++
@@ -1,8 +0,0 @@
-function view(data){
-    let canvas = document.getElementById("paint");
-    let ctx = canvas.getContext("2d")
-    let newImg = document.createElement("img");
-    
-    newImg.src = data;
-    document.body.appendChild(newImg);
-}
